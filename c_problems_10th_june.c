1177f24d080190edb032d60d0feaa667d6faedbf
Tabdeel Studios <aquib@tabdeel.in>
2021-06-10 20:53:11 +0530

    Add files via upload

---
+++ c_problems_10th_june.c
@@ -0,0 +1,60 @@
+//1. Write a program to return the
+//maximum occurring character in an input string.
+
+Input : intelligent
+Output : i (2)
+
+Step 1 : Find the length of the string
+Step 2 : 
+
+arr1 = "intelligent"
+arr2[0] = 'i'
+arr2[1] = 'n'
+arr2[2] = 't'
+
+for(i=0,j=0; i!='\0'; i++)
+{
+	if(arr2[j] == arr1[i])
+	{
+		count[i]++;
+	}
+}
+
+
+//2. Write a program that takes in a letter class ID of a ship and display the equivalent string class description of the given ID. Use the table below.
+
+Step 1. Read the input string
+Step 2. Compare using switch or strcmp (string.h)
+Step 3. Display the output
+
+
+//3. Good sequence problem
+a. Natural numbers/integers
+b. Ascending order
+c. Maximum number = n; arr[4] = 5
+
+Input : 
+5
+1,  2,   3,  4,  5
+A1, A2, A3, A4, A5
+
+A1-1 = 0
+A2-2 = 0
+A3-3 = 0
+
+//4. Crayon problem
+
+if(a[i] == 'U' && a[i+1]=='D')
+{
+	count++;
+}
+else if(a[i] == 'U' && a[i+1]=='U')
+	continue;
+else if(a[i] == 'D' && a[i+1]=='D')
+	continue;
+
+
+
+
+
+
